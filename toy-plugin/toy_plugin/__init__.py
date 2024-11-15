88b368ee4c03abb6e81d105320cfec60b7776925
Draga Doncila Pop <ddoncila@gmail.com>
2024-11-15 17:20:38 +1100

    Add toy plugin example

---
+++ toy-plugin/toy_plugin/__init__.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+from magicgui import magic_factory
+from skimage.morphology import disk
+from skimage.filters.rank import mean
+
+@magic_factory(
+    auto_call=True,
+    threshold={"widget_type": "FloatSlider", "max": 1}
+)
+def filter_and_threshold(
+    layer: 'napari.layers.Image',
+    disk_size: int,
+    threshold: float
+) -> 'napari.types.LayerDataTuple':
+    
+    layer_tuples = []
+    filtered_image = layer.data
+    image_meta = {}
+    
+    if disk_size > 0:
+        filter_disk = disk(disk_size)
+        filtered_image = mean(layer.data, footprint=filter_disk)
+        image_meta['name'] = 'Filtered'
+        image_meta['visible'] = True
+        layer_tuples.append(
+            (filtered_image, image_meta, 'image')
+        )
+        
+    if threshold > 0:
+        scaled_threshold = threshold * np.max(layer.data)
+        thresholded_labels = (filtered_image > scaled_threshold).astype(np.uint8)
+        labels_meta = {
+            'name': 'Thresholded',
+            'visible': True
+        }
+        image_meta['visible'] = False
+        layer_tuples.append(
+            (thresholded_labels, labels_meta, 'labels')
+        )
+    return layer_tuples
+
+if __name__ == '__main__':
+    import napari
+
+    viewer = napari.Viewer()
+    viewer.open_sample('napari', 'human_mitosis')
+    viewer.window.add_dock_widget(filter_and_threshold())
+
+    napari.run()
